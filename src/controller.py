e000cece951ba4c15cf95fe3511fbe7edcb7491a
Martin Endler <pokusew@seznam.cz>
2023-01-10 02:53:44 +0100

    Improve initialization (make it more robust) and implement git pull and git push with auto rebase on conflict in GithubGistClient

--- src/controller.py
+++ src/controller.py
@@ -3,9 +3,11 @@ import os
 import select
 import shutil
 import sys
-from time import sleep
 
 from git import GithubGistClient
+
+# from time import sleep
+# import subprocess
 
 IMAGES_LIBRARY = '4c7fe7e6d06b0b90ab4848b234209e95'
 CONTROL_IMAGE = 'security.jpg'
@@ -14,54 +16,104 @@ UPDATE_INTERVAL = 30  # seconds
 
 class Controller:
 
-    def __init__(self, workdir: str, gist: str, token: str) -> None:
+    def __init__(
+        self,
+        workdir: str,
+        gist: str,
+        token: str,
+        recreate_workdir: bool = False,
+        skip_init_reset: bool = False,
+        skip_init_pull: bool = False,
+    ) -> None:
         self._workdir = os.path.abspath(workdir)
         self._gist = gist
         self._token = token
-
-    def _setup(self) -> None:
-        print(f'removing and re-creating workdir {self._workdir}...')
-        shutil.rmtree(self._workdir)
-        os.makedirs(self._workdir)
-        print(f'workdir ready')
+        self._recreate_workdir = recreate_workdir
+        self._skip_init_reset = skip_init_reset
+        self._skip_init_pull = skip_init_pull
 
         self._lib_dir = os.path.join(self._workdir, 'lib')
-        self._comm_dir = os.path.join(self._workdir, 'comm')
-
-        print('downloading images library to...')
         self._lib_client = GithubGistClient(
             gist=IMAGES_LIBRARY,
             repo_dir=self._lib_dir,
         )
-        self._lib_client.clone()
-        self._lib_images = set(filter(
-            lambda f: f.endswith('.png') or f.endswith('.jpg'),
-            os.listdir(self._lib_dir),
-        ))
-        print(f'library contains {len(self._lib_images)} images')
-        for img in self._lib_images:
-            print(f'  {img}')
-        if CONTROL_IMAGE not in self._lib_images:
-            raise RuntimeError(f'Control image {CONTROL_IMAGE} is not the library!')
 
+        self._comm_dir = os.path.join(self._workdir, 'comm')
         self._comm_client = GithubGistClient(
             gist=self._gist,
             token=self._token,
             repo_dir=self._comm_dir,
         )
-        print(f'cloning the gist from {self._comm_client.get_https_url(with_token=False)} to {self._comm_dir} ...')
-        self._comm_client.clone()
+
+    def _create_commands_parser(self) -> None:
+        self._parser = argparse.ArgumentParser()
+        self._parser.add_argument(
+            'workdir',
+            help='work directory where the files and directories created by the controller will be put'
+        )
+        self._parser.add_argument('gist', help='ID of the GitHub Gist for communication with bots')
+        self._parser.add_argument('token', help='GitHub Personal access token (classic) with (at least) gist scope')
+        return
+
+    def _ensure_workdir(self) -> None:
+        if os.path.isdir(self._workdir) and self._recreate_workdir:
+            print(f'removing and re-creating workdir {self._workdir}...')
+            shutil.rmtree(self._workdir)
+        if not os.path.isdir(self._workdir):
+            os.makedirs(self._workdir)
+        print(f'workdir ready')
+
+    def _load_lib_images(self):
+        self._lib_images = set(filter(
+            lambda f: f.endswith('.png') or f.endswith('.jpg'),
+            os.listdir(self._lib_dir),
+        ))
+        print(f'library contains {len(self._lib_images)} images:')
+        for img in self._lib_images:
+            print(f'  {img}')
+        if CONTROL_IMAGE not in self._lib_images:
+            raise RuntimeError(f'Control image {CONTROL_IMAGE} is not the library!')
+
+    def _setup(self) -> None:
+        self._ensure_workdir()
+
+        print('initializing images library to...')
+        self._lib_client.init(
+            skip_reset=self._skip_init_reset,
+            skip_pull=self._skip_init_pull,
+        )
+        self._load_lib_images()
+
+        print(f'initializing the gist from {self._comm_client.get_https_url(with_token=False)} to {self._comm_dir} ...')
+        self._comm_client.init(
+            skip_reset=self._skip_init_reset,
+            skip_pull=self._skip_init_pull,
+        )
 
         self._bots = {}
+
+        self._create_commands_parser()
 
         pass
 
     def update_state(self) -> None:
         print('updating bots state ...')
+        self._comm_client.pull_changes()
+        # TODO
         pass
 
     def process_command(self, cmd_str) -> None:
         print(f"processing command '{cmd_str}'")
+        args = self._parser.parse_args(args=cmd_str.split(sep=' '))
+        print(args)
+        # help
+        # terminate <bot>
+        # do <bot> id
+        # do <bot> who
+        # do <bot> ls <>
+        # run [--shell] [--no-output] <bot>
+        # copyFrom <bot> <file name>
+
         pass
 
     def run(self) -> None:
@@ -73,6 +125,10 @@ class Controller:
             if len(ready_read) == 1:
                 cmd_str = sys.stdin.readline().strip()
                 self.process_command(cmd_str)
+            else:
+                # so we do not write to the prompt line
+                # note: we could also use ASCII ESC sequences to clear the prompt (and add colors as well)
+                print('')
             self.update_state()
 
         pass
@@ -85,20 +141,49 @@ class Controller:
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
+    _parser = argparse.ArgumentParser()
+    _parser.add_argument(
         'workdir',
-        help='work directory where the files and directories created by the controller will be put'
+        help='work directory where the files and directories created by the controller will be put',
     )
-    parser.add_argument('gist', help='ID of the GitHub Gist for communication with bots')
-    parser.add_argument('token', help='GitHub Personal access token (classic) with (at least) gist scope')
-    args = parser.parse_args()
-    controller = Controller(
-        workdir=args.workdir,
-        gist=args.gist,
-        token=args.token
+    _parser.add_argument(
+        'gist',
+        help='ID of the GitHub Gist for communication with bots',
+    )
+    _parser.add_argument(
+        'token',
+        help='GitHub Personal access token (classic) with (at least) gist scope',
+    )
+    _parser.add_argument(
+        '--recreate',
+        action='store_true',
+        help='Remove and recreate the workdir even if it already exists',
+    )
+    _parser.add_argument(
+        '--skip-init-reset',
+        action='store_true',
+        help='Skip resetting the git repos if they already exist.',
+    )
+    _parser.add_argument(
+        '--skip-init-pull',
+        action='store_true',
+        help='Skip updating the git repos if they already exist.',
+    )
+    _parser.add_argument(
+        '--fast-init',
+        action='store_true',
+        help='Shortcut for combination of of --skip-init-reset and --skip-init-pull.',
+    )
+    _args = _parser.parse_args()
+    _controller = Controller(
+        workdir=_args.workdir,
+        gist=_args.gist,
+        token=_args.token,
+        recreate_workdir=_args.recreate,
+        skip_init_reset=_args.fast_init or _args.skip_init_reset,
+        skip_init_pull=_args.fast_init or _args.skip_init_pull,
     )
     try:
-        controller.run()
+        _controller.run()
     except KeyboardInterrupt:
-        controller.stop()
+        _controller.stop()
--- src/git.py
+++ src/git.py
@@ -1,6 +1,7 @@
 import os
+import shutil
 import subprocess
-from typing import Optional
+from typing import Optional, List
 
 
 class GithubGistClient:
@@ -14,6 +15,32 @@ class GithubGistClient:
         token = self._token + '@' if (with_token and self._token is not None) else ''
         return f"https://{token}gist.github.com/{self._gist}.git"
 
+    def verify(self) -> bool:
+        if not os.path.isdir(self._repo_dir):
+            return False
+        p = subprocess.run(
+            args=[
+                'git',
+                'remote',
+                'get-url',
+                'origin'
+            ],
+            encoding='utf-8',
+            cwd=self._repo_dir,
+            capture_output=True,
+        )
+        if p.returncode != 0:
+            return False
+        if p.stdout.rstrip() != self.get_https_url(with_token=True):
+            return False
+        return True
+
+    def git_check_call(self, args: List[str]) -> None:
+        subprocess.check_call(
+            args=['git'] + args,
+            cwd=self._repo_dir,
+        )
+
     def clone(self) -> None:
         full_url = self.get_https_url(with_token=True)
         subprocess.check_call(
@@ -26,22 +53,115 @@ class GithubGistClient:
             cwd=os.path.dirname(self._repo_dir),
         )
 
-    def pull(self) -> None:
-        subprocess.check_call(
+    def init(self, skip_reset: bool = False, skip_pull: bool = False) -> None:
+        if self.verify():
+            # git repo exists, just throw away all possible local changes and pull the latest remote
+            if not skip_reset:
+                self.git_check_call(['reset', '--hard', 'origin/main'])
+            if not skip_pull:
+                self.pull_changes()
+            return
+        # either the repo does not exist or it is invalid
+        # first, make sure to delete whatever is in the place of the repo dir
+        if os.path.isdir(self._repo_dir):
+            shutil.rmtree(self._repo_dir)
+        # it might be a file
+        if os.path.exists(self._repo_dir):
+            os.remove(self._repo_dir)
+        # finally, we can clone the repo
+        self.clone()
+
+    def add(self, pathspecs: List[str]) -> bool:
+        code = subprocess.call(
+            args=['git', 'add'] + pathspecs,
+            cwd=self._repo_dir,
+        )
+        return code == 0
+
+    def commit(self, message='Update') -> bool:
+        code = subprocess.call(
+            args=['git', 'commit', '-m', message],
+            cwd=self._repo_dir,
+        )
+        return code == 0
+
+    def _pull(self) -> bool:
+        code = subprocess.call(
             args=[
                 'git',
                 'pull',
+                '--ff-only',
             ],
             cwd=self._repo_dir,
         )
+        return code == 0
 
-    def push(self) -> None:
-        subprocess.check_call(
+    def _fetch(self) -> bool:
+        code = subprocess.call(
+            args=[
+                'git',
+                'fetch',
+            ],
+            cwd=self._repo_dir,
+        )
+        return code == 0
+
+    def _rebase(self) -> bool:
+        code = subprocess.call(
+            args=[
+                'git',
+                'rebase',
+            ],
+            cwd=self._repo_dir,
+        )
+        if code != 0:
+            subprocess.call(
+                args=[
+                    'git',
+                    'rebase',
+                    '--abort'
+                ],
+                cwd=self._repo_dir,
+            )
+        return code == 0
+
+    def _push(self) -> bool:
+        code = subprocess.call(
             args=[
                 'git',
                 'push',
             ],
             cwd=self._repo_dir,
         )
+        return code == 0
+
+    def pull_changes(self, max_retries: int = 2) -> None:
+        # we do not anticipate any conflicts as we always pull changes
+        # only when all local changes have been pushed first
+        # the retries are here only to handle unexpected transient failures (e.g. network)
+        for i in range(0, max_retries):
+            if self._pull():
+                return
+        raise RuntimeError(f'pull_changes failed (max_retries={max_retries})')
+
+    def push_changes(self, max_retries: int = 3) -> None:
+        for i in range(0, max_retries):
+            # try push
+            if self._push():
+                # successful, we are done
+                print(f'push_changes succeeded attempt={i}')
+                return
+            # push failed probably, because of a conflict (we could parse the stderr of git push to be sure)
+            # let's try to fetch the latest
+            if not self._fetch():
+                # execute error, retry the whole sequence
+                continue
+            if not self._rebase():
+                # git rebase should normally never fail
+                # if there is nothing to rebase it should also exit with 0 and "Current branch main is up to date.
+                raise RuntimeError('git rebase failed')
+            # continue with attempting to push
+            continue
+        raise RuntimeError(f'push_changes failed (max_retries={max_retries})')
 
     pass
